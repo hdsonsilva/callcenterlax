b962e0d6bdb3e46e74465b9d136229c5738bfdf9
Hudson Silva <hudson@doctum.edu.br>
2016-12-17 12:57:17 -0200

    OK

--- js/my-app.js
+++ js/my-app.js
@@ -127,95 +127,99 @@ function getConfig(){
 	return 1;
 }
 
+
+	var myApp = new Framework7();
+
+	// Export selectors engine
+	var $$ = Dom7;
 // Initialize your app
-var myApp = new Framework7();
+$$(document).on('DOMContentLoaded',function(){
 
-// Export selectors engine
-var $$ = Dom7;
-
-// Add views
-var view1 = myApp.addView('#view-1', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
-});
-var view2 = myApp.addView('#view-2');
-var view3 = myApp.addView('#view-3');
-var view4 = myApp.addView('#view-4');
-var view5 = myApp.addView('#view-5');
+	// Add views
+	var view1 = myApp.addView('#view-1', {
+	    // Because we use fixed-through navbar we can enable dynamic navbar
+	    dynamicNavbar: true
+	});
+	var view2 = myApp.addView('#view-2');
+	var view3 = myApp.addView('#view-3');
+	var view4 = myApp.addView('#view-4');
+	var view5 = myApp.addView('#view-5');
 
 
 
-//Verificando se as configurações existem no inicio da aplicação
-if(!getConfig()){
-	alert('É necessário configurar o App');
-	myApp.showTab('#view-4');
-}
-//Verificando se as configurações existem quando se alterar o menu
-$$('#view-1, #view-2, #view-3').on('show',function(){
+	//Verificando se as configurações existem no inicio da aplicação
 	if(!getConfig()){
 		alert('É necessário configurar o App');
 		myApp.showTab('#view-4');
 	}
-});
-//Setando visualizacao inicial do aplicativo
-myApp.showTab('#view-2');
-//Iniciando parametros de conexao
-initparametros();
-//Fazendo primeira busca dos dados no servidor
-updateview1();
-//Setando periodo de atualização dos dados para 5 segundos
-window.setInterval(updateview1,5000);
+	//Verificando se as configurações existem quando se alterar o menu
+	$$('#view-1, #view-2, #view-3').on('show',function(){
+		if(!getConfig()){
+			alert('É necessário configurar o App');
+			myApp.showTab('#view-4');
+		}
+	});
 
-//Preenchendo os dados da aba de resumo de campanhas
-$$('#view-1').on('show' , function(e){
-	//window.setInterval(updateview1(),5000);
-});
+	//Setando visualizacao inicial do aplicativo
+	myApp.showTab('#view-2');
+	//Iniciando parametros de conexao
+	initparametros();
+	//Fazendo primeira busca dos dados no servidor
+	updateview1();
+	//Setando periodo de atualização dos dados para 5 segundos
+	window.setInterval(updateview1,5000);
 
-//Preenchendo os campos de conexão com o servidor se existirem
-$$('#view-4').on('show' , function(e){
+	//Preenchendo os dados da aba de resumo de campanhas
+	$$('#view-1').on('show' , function(e){
+		//window.setInterval(updateview1(),5000);
+	});
 
-	$$('#__user__').val(localStorage.getItem('__user__'));
-	$$('#__passwd__').val(localStorage.getItem('__passwd__'));
-	$$('#__url__').val(localStorage.getItem('__url__'));
+	//Preenchendo os campos de conexão com o servidor se existirem
+	$$('#view-4').on('show' , function(e){
 
-});
+		$$('#__user__').val(localStorage.getItem('__user__'));
+		$$('#__passwd__').val(localStorage.getItem('__passwd__'));
+		$$('#__url__').val(localStorage.getItem('__url__'));
 
-//Buscando campanhas do sistema
-$$('#link-campanha').click(function(){
+	});
 
-	
-});
+	//Buscando campanhas do sistema
+	$$('#link-campanha').click(function(){
 
-//Salvando configurações de acesso ao servidor elastix
-$$('#salvarconfig').click(function(){
-	alert('http://' + $$('#__url__').val()+ '/elastixserver/autenticacao.php');
-	$$.get(
-		'http://' + $$('#__url__').val()+ '/elastixserver/autenticacao.php',
-		{	
-			'__user__'	:$$('#__user__').val(),
-			'__passwd__':$$('#__passwd__').val()
-		},
-		function(valor){
+		
+	});
 
-			if( valor == '' ){
+	//Salvando configurações de acesso ao servidor elastix
+	$$('#salvarconfig').click(function(){
+		$$.get(
+			'http://' + $$('#__url__').val()+ '/elastixserver/autenticacao.php',
+			{	
+				'__user__'	:$$('#__user__').val(),
+				'__passwd__':$$('#__passwd__').val()
+			},
+			function(valor){
+
+				if( valor == '' ){
+					localStorage.setItem('__user__', $$('#__user__').val());
+					localStorage.setItem('__passwd__', $$('#__passwd__').val());
+					localStorage.setItem('__url__', $$('#__url__').val());
+
+					alert('Ok! Dados configurados com sucesso!');
+				}
+				else{
+					alert('Erro! Credenciais inválidas!');	
+				}
+			},
+			function(e){
+				
 				localStorage.setItem('__user__', $$('#__user__').val());
 				localStorage.setItem('__passwd__', $$('#__passwd__').val());
 				localStorage.setItem('__url__', $$('#__url__').val());
+				alert('Erro na conexão.');
+			}
+		);
+	});
 
-				alert('Ok! Dados configurados com sucesso!');
-			}
-			else{
-				alert('Erro! Credenciais inválidas!');	
-			}
-		},
-		function(e){
-			
-			localStorage.setItem('__user__', $$('#__user__').val());
-			localStorage.setItem('__passwd__', $$('#__passwd__').val());
-			localStorage.setItem('__url__', $$('#__url__').val());
-			alert('Erro na conexão.');
-		}
-	);
 });
 /*
 $$(document).on('pageInit', function (e) {
