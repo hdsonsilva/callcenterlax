118a7c089100d8b8b3f0ff0cce3be5bf75cce0b8
Hudson Silva <hudson@doctum.edu.br>
2017-01-02 17:44:14 -0200

    Colocando icone no projeto

--- www/dist/js/my-app.js
+++ www/dist/js/my-app.js
@@ -2,6 +2,7 @@ var myApp = new Framework7();
 
 // Export selectors engine
 var $$ = Dom7;
+
 // Initialize your app
 $$(document).on('DOMContentLoaded',function(){
 
@@ -22,6 +23,8 @@ $$(document).on('DOMContentLoaded',function(){
 		alert('É necessário configurar o App');
 		myApp.showTab('#view-4');
 	}
+
+
 	//Verificando se as configurações existem quando se alterar o menu
 	$$('#view-1, #view-2, #view-3').on('show',function(){
 		if(!getConfig()){
@@ -31,7 +34,7 @@ $$(document).on('DOMContentLoaded',function(){
 	});
 
 	//Setando visualizacao inicial do aplicativo
-	myApp.showTab('#view-2');
+	myApp.showTab('#view-1');
 	//Iniciando parametros de conexao
 	initparametros();
 	//Fazendo primeira busca dos dados no servidor
@@ -43,21 +46,79 @@ $$(document).on('DOMContentLoaded',function(){
 	$$('#view-1').on('show' , function(e){
 		//window.setInterval(updateview1(),5000);
 	});
+	//Buscando campanhas para exibir quando o usuario selecionar o menu de campanhas
+	$$('#view-5').on('show' , function(e){
 
-	//Preenchendo os campos de conexão com o servidor se existirem
+		//window.setInterval(updateview1(),5000);
+		$$.getJSON(
+			'http://' + $$('#__url__').val()+ '/elastixserver/',
+			{	
+				'__user__'	:$$('#__user__').val(),
+				'__passwd__':$$('#__passwd__').val()
+			},
+			function(jsonn){
+				$$('#campanhasdetalhes').html("<ul>");
+				$$.each(jsonn.Campanhas, function(index, value){ 
+				     $$('#campanhasdetalhes').html($$('#campanhasdetalhes').html()+ "<li><div class='item-content'><div class='item-inner'><div class='item-title'> "+value.name+" </div></div></div></li>");
+				});
+				$$('#campanhasdetalhes').html($$('#campanhasdetalhes').html() + "</ul>");
+		
+			}
+		);
+
+	});
+
+	$$('#view-6').on('show' , function(e){
+
+		//window.setInterval(updateview1(),5000);
+		$$.getJSON(
+			'http://' + $$('#__url__').val()+ '/elastixserver/',
+			{	
+				'__user__'	:$$('#__user__').val(),
+				'__passwd__':$$('#__passwd__').val()
+			},
+			function(jsonn){
+				$$('#agentesdetalhes').html("<ul>");
+				$$.each(jsonn.Agentes, function(index, value){ 
+				     $$('#agentesdetalhes').html($$('#agentesdetalhes').html()+ "<li><div class='item-content'><div class='item-inner'><div class='item-title'> "+value.number + ' - ' + value.name + ( value.id_break ? ' | Parada Solicitada' : '') +" </div></div></div></li>");
+				});
+				$$('#agentesdetalhes').html($$('#agentesdetalhes').html() + "</ul>");
+		
+			}
+		);
+
+	});
+
+	//Preenchendo os campos de conexão com o servidor se existirem para exibir para o usuario os campos preenchidos
 	$$('#view-4').on('show' , function(e){
 
 		$$('#__user__').val(localStorage.getItem('__user__'));
 		$$('#__passwd__').val(localStorage.getItem('__passwd__'));
 		$$('#__url__').val(localStorage.getItem('__url__'));
+		$$('#__campanha__').val(localStorage.getItem('__campanha__'));
+
 
 	});
+	//Preenchendo lista de campanhas ativas na configuração 
+	$$.getJSON(
+		'http://' + $$('#__url__').val()+ '/elastixserver/',
+		{	
+			'__user__'	:$$('#__user__').val(),
+			'__passwd__':$$('#__passwd__').val()
+		},
+		function(jsonn){
+			 $$('#__campanha__').html("<option value=''>Todas</option>");
+			$$.each(jsonn.Campanhas, function(index, value){ 
+			     $$('#__campanha__').html($$('#__campanha__').html()+"<option value='"+value.id+"'>"+value.id +" | "+value.name+"</option>");
+			});
 
-	//Buscando campanhas do sistema
-	$$('#link-campanha').click(function(){
-
-		
-	});
+	
+		}
+	);
+	//Carregando id da campanha
+	 $$('#__campanha__').change(function(){
+	 	localStorage.setItem('__campanha__',$$(this).val());
+	 });
 
 	//Salvando configurações de acesso ao servidor elastix
 	$$('#salvarconfig').click(function(){
--- www/dist/js/person.js
+++ www/dist/js/person.js
@@ -11,6 +11,7 @@ function initparametros(){
 
 
 function updateview1(){
+	var totalligacoes = 0 ;
 	$$.getJSON(
 		'http://' + $$('#__url__').val()+ '/elastixserver/',
 		{	
@@ -19,8 +20,9 @@ function updateview1(){
 		},
 		function(jsonn){
 			
-			$$('#numagentes').html("<span class='badge bg-red'>"+jsonn.AgentesAtivos+"</span>");
-			$$('#numcampanhas').html("<span class='badge bg-red'>"+jsonn.CampanhasAtivas+"</span>");
+			$$('#numagentes').html("<span class='badge bg-red'>"+(jsonn.Agentes.length != undefined ? jsonn.Agentes.length : 0) +"</span>");
+			$$('#numcampanhas').html("<span class='badge bg-red'>"+(jsonn.Campanhas.length != undefined ? jsonn.Campanhas.length : 0)+"</span>");
+
 			atualizahoralast()
 		}
 	);
@@ -39,10 +41,10 @@ function updateview1(){
 			$$.each(jsonn.Discadora, function(index, value){ 
 
 			     $$('#listadiscadora').html($$('#listadiscadora').html()+"<li><div class='item-content'><div class='item-inner'> <div class='item-title itemtitleperson'><div class='alinhamentoesquerda'><i class='f7-icons'>search_strong</i> "+value.trunk + "</div><div class='alinhamentodireita'><i class='f7-icons'>phone</i> "+value.phone+"</div></div></div></div></li>");
-			  });
+			});
 			$$.each(jsonn.Agentes, function(index, value){ 
 				$$('#listachamadas').html($$('#listachamadas').html()+"<li><div class='item-content'><div class='item-inner'> <div class='item-title  itemtitleperson'><div class='alinhamentoesquerda'><i class='f7-icons'>person_fill</i> "+value.nomeagente + "</div><div class='alinhamentodireita'><i class='f7-icons'>phone_fill</i> "+value.phone+"</div></div></div></div></li>");     
-			  });
+			});
 		}
 	);
 	/*Buscando resumo das chamadas do dia 
@@ -52,71 +54,82 @@ function updateview1(){
 	Nao atendidas NoAnswer
 	Curtas ShortCall
 	*/
+		
+	//Chamadas Curtas
 	$$.getJSON(
 		'http://' + $$('#__url__').val()+ '/elastixserver/chamadasdodia.php',
 		{	
 			'__user__'	:$$('#__user__').val(),
 			'__passwd__':$$('#__passwd__').val(),
-			'status'	:'Success'
+			'status'	:'ShortCall',
+			'sintetico'	: '1'
 		},
 		function(jsonn){
-			$$('#chamadastotalsucesso').html(jsonn.length+'<br>Sucesso');
-			
-		}
-	);
-	$$.getJSON(
-		'http://' + $$('#__url__').val()+ '/elastixserver/chamadasdodia.php',
-		{	
-			'__user__'	:$$('#__user__').val(),
-			'__passwd__':$$('#__passwd__').val(),
-			'status'	:'NoAnswer'
-		},
-		function(jsonn){
-			var total = parseInt(jsonn.length);
-
+			$$('#chamadastotalcurtas').html(jsonn.Total+'<br>Curtas');
+			totalligacoes += parseInt(jsonn.Total);
+			//Chamadas Falhas
 			$$.getJSON(
 				'http://' + $$('#__url__').val()+ '/elastixserver/chamadasdodia.php',
 				{	
 					'__user__'	:$$('#__user__').val(),
 					'__passwd__':$$('#__passwd__').val(),
-					'status'	:'Abandoned'
+					'status'	:'Failure',
+					'sintetico'	: '1'
 				},
 				function(jsonn){
-					total = total + parseInt(jsonn.length);
+					$$('#chamadastotalfalhas').html(jsonn.Total+'<br>Falhas');
+					totalligacoes += parseInt(jsonn.Total);
+					//Chamadas nao respondidas
+					$$.getJSON(
+						'http://' + $$('#__url__').val()+ '/elastixserver/chamadasdodia.php',
+						{	
+							'__user__'	:$$('#__user__').val(),
+							'__passwd__':$$('#__passwd__').val(),
+							'status'	:'NoAnswer',
+							'sintetico'	: '1'
+						},
+						function(jsonn){
+							var total = parseInt(jsonn.Total);
 
-					$$('#chamadastotalnaoatendidas').html(total+'<br>Perdidas');
-					
+							$$.getJSON(
+								'http://' + $$('#__url__').val()+ '/elastixserver/chamadasdodia.php',
+								{	
+									'__user__'	:$$('#__user__').val(),
+									'__passwd__':$$('#__passwd__').val(),
+									'status'	:'Abandoned',
+									'sintetico'	: '1'
+								},
+								function(jsonn){
+									total = total + parseInt(jsonn.Total);
+									totalligacoes += parseInt(total);
+									$$('#chamadastotalnaoatendidas').html(total+'<br>Perdidas');
+									//Chamadas com sucesso
+									$$.getJSON(
+											'http://' + $$('#__url__').val()+ '/elastixserver/chamadasdodia.php',
+											{	
+												'__user__'	:$$('#__user__').val(),
+												'__passwd__':$$('#__passwd__').val(),
+												'status'	:'Success',
+												'sintetico'	: '1'
+											},
+											function(jsonn){
+												$$('#chamadastotalsucesso').html(jsonn.Total+'<br>Sucesso');
+												totalligacoes += parseInt(jsonn.Total);
+												$$('#totaldechamadas').html(totalligacoes+'<br>Geral');
+											}
+										);
+								}
+							);
+							
+						}
+					);
+
 				}
 			);
-			
 		}
 	);
 
-	$$.getJSON(
-		'http://' + $$('#__url__').val()+ '/elastixserver/chamadasdodia.php',
-		{	
-			'__user__'	:$$('#__user__').val(),
-			'__passwd__':$$('#__passwd__').val(),
-			'status'	:'ShortCall'
-		},
-		function(jsonn){
-			$$('#chamadastotalcurtas').html(jsonn.length+'<br>Curtas');
-			
-		}
-	);
-
-	$$.getJSON(
-		'http://' + $$('#__url__').val()+ '/elastixserver/chamadasdodia.php',
-		{	
-			'__user__'	:$$('#__user__').val(),
-			'__passwd__':$$('#__passwd__').val(),
-			'status'	:'Failure'
-		},
-		function(jsonn){
-			$$('#chamadastotalfalhas').html(jsonn.length+'<br>Falhas');
-			
-		}
-	);
+	
 	
 }
 //Função que verifica se as configurações foram setadas
