ecd3b248ab22042bbf260a2c47a6f605cdfecc62
Hudson Silva <hudson@doctum.edu.br>
2017-01-10 10:56:42 -0200

    Incluindo chave de comunicacao com push notification

--- www/dist/js/my-app.js
+++ www/dist/js/my-app.js
@@ -335,27 +335,20 @@ $$(document).on('DOMContentLoaded',function(){
 	});
 
 	// Configura a notificação, altere o senderID
-	var push = PushNotification.init({
-	  "android": {"senderID": "182505207980", icon : "icon"},
-	  "ios": {"alert": "true", "badge": "true", "sound": "true"},
-	  "windows": {}
-	});
 
-	// veja a explicação seguir
-	push.on('registration', function(data) {
-	   alert(data.registrationId);
-	});
+	var pushNotification = window.plugins.pushNotification;
+	pushNotification.register(
+		function(result) {
+    		alert('Callback Success! Result = '+result)
+    	}, 
+		function(error) {
+    		alert(error)
+    	},
+		{"senderID":"182505207980","ecb":"app.onNotificationGCM"}
+	);
 
-	// O que fazer quando clicar em uma notificação
-	push.on('notification', function(data) {
-	  alert('Notificação acionada, agora deve-se implementar a navegação no app de acordo com os dados: ' + JSON.stringify(data));
-	});
 
-	// erro caso não possa registrar (veja a explicação seguir)
-	push.on('error', function(e) {
-	  alert('registration error: ' + e.message);
-	});
-
+	
 
 });
 /*
