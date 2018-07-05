91c199d9c5b2c5d72bcd8420d1d624e2128831ff
Hudson <hudson@doctum.edu.br>
2018-07-04 21:26:51 -0300

    alterando onsignal de 2.4 para 2.3

--- www/dist/js/my-app.js
+++ www/dist/js/my-app.js
@@ -10,13 +10,12 @@ $$(document).on('DOMContentLoaded',function(){
 	
 	// Add to index.js or the first page that loads with your app.
 	// For Intel XDK and please add this to your app.js.
-
 	document.addEventListener('deviceready', function () {
 	  // Enable to debug issues.
 	  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
 	  
 	  var notificationOpenedCallback = function(jsonData) {
-	    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+	    //console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
 	  };
 
 	  window.plugins.OneSignal
